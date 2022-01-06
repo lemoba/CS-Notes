52a11da86e5670301fa209b9beca55fc53c1fcd1
ranen <lemoba@163.com>
2022-01-06 22:43:06 +0800

    add pthread

--- ostep/3-Concurrency/1-threads-intro/t1.c
+++ ostep/3-Concurrency/1-threads-intro/t1.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-a
+
 #include "../includes/common_threads.h"
 #include "../includes/common.h"
 
 int max;
 volatile int counter = 0; // shared global variable
 
-void *mythread(void *arg) {
+void *mythread(void *arg) 
+{
     char *letter = arg;
     int i;
     printf("%s: begin [addr of i: %p]\n", letter, &i);
+
+    pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+    Pthread_mutex_lock(&lock);
     for (i = 0; i < max; i++) {
         counter += 1;
     }
+    Pthread_mutex_unlock(&lock);
     printf("%s: done\n", letter);
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
     if (argc != 2) {
         fprintf(stderr, "usage: main-first <loopcount>\n");
         exit(1);
@@ -34,6 +40,7 @@ int main(int argc, char *argv[]) {
 
     Pthread_join(p1, NULL);
     Pthread_join(p2, NULL);
+    
     printf("main: done\n [counter: %d]\n [should: %d]\n", counter, max*2);
     return 0;
 }
